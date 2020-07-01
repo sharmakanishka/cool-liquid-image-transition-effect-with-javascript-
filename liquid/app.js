9851f96fbf87b060e4f8d8d001e8f456b6fd1850
Kanishka Sharma <67273521+sharmakanishka@users.noreply.github.com>
2020-07-01 16:13:36 +0530

    Add files via upload

---
+++ liquid/app.js
@@ -0,0 +1,14 @@
+try{
+    new hoverEffect({
+    parent: document.querySelector(".distortion"),
+    intensity:0.2,
+    image1:"./topless-woman-with-eyes-close-1164980.jpg",
+    image2:"./grayscale-photo-of-topless-woman-1164674.jpg",
+    angle: Math.PI/4,
+    speedIn:2,
+    speedOut:5,
+    displacementImage:"./stripe1.png"
+}); }
+catch(exception){
+    console.log(exception);
+}
